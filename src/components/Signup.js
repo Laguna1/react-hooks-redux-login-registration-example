b976b8e6d01dfea5b6eab3ea8369aefa8fd78d9b
Laguna1 <lagunazoomarket@gmail.com>
2021-08-06 02:25:35 +0300

    Add register form to sign up page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import FormInput from './FormInput'
+import SubmitButton from './SubmitBtn'
 
 const Signup = () => {
     return (
-        <div>
-            
+        <div className="signup">
+          <small>Register, please</small>
+          <form>
+                <FormInput
+                name="username"
+                type="username"
+                placeholder="username" 
+
+                />
+                <FormInput
+                name="password"
+                type="password"
+                placeholder="password"
+                />
+                <FormInput
+                name="confirmPassword"
+                type="confirmPassword"
+                placeholder="confirmPassword"
+                />
+                <SubmitButton>Sign Up</SubmitButton>
+            </form>
         </div>
     )
 }
